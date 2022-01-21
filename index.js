03f4a59903498b75cfec1d69c02cd30252f5713c
Kevin Wang <kevqwang@gmail.com>
2022-01-21 14:28:53 -0500

    commit tree 09212944007265543c1775ae0229aa0a29fb713d

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
+function reverse(word) {
+  // 'abc' => 'cba'
+  // const wordArray = word.split("");
+  // const reversedWordArray = wordArray.reverse();
+  // const reversedWord = reversedWordArray.join("");
+  // return reversedWord;
+  return word.split("").reverse().join("");
+}
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  const reversedWord = reverse(word);
+
+  return word === reversedWord;
 }
 
 /* 
   Add your pseudocode here
 */
+// check if the inputed word is spelled the same forward as it is backward
+//check index of the character value at the start [0] and compare it to the string in reverse at -1 then increment over maybe with a for loop
 
 /*
   Add written explanation of your solution here
